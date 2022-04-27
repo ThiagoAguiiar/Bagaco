b3097b0186b2a2fc2ec11991093658abd1c542d2
yass_ <yas.coutom@gmail.com>
2022-04-27 16:29:27 -0300

    commit banco dados

---
+++ Ecommerce/Ecommerce/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Entrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public string Entrar(string cpf, string senha)
+        {
+            Usuario u = new Usuario(cpf, senha);
+            TempData["msg"] = u.Entra(cpf, senha);
+
+            return 
+         
+
+        }
+    }
+}
--- Ecommerce/Ecommerce/Models/Usuario.cs
+++ Ecommerce/Ecommerce/Models/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,31 +8,61 @@ namespace Ecommerce
 {
     public class Usuario
     {
+        static string conexao = "Server=ESN509VMYSQL;Database=bagaco;User id=aluno;Password=Senai1234";
+
+        
         private string nome;
         private string telefone;
         private string email;
         private string tipo;
+        private string cpf;
+        private string senha;
+
+        public Usuario(string cpf, string senha)
+        {
+            this.senha = senha;
+            this.cpf = cpf;
+        }
 
         public string Nome { get => nome; set => nome = value; }
         public string Telefone { get => telefone; set => telefone = value; }
         public string Email { get => email; set => email = value; }
         public string Tipo { get => tipo; set => tipo = value; }
+        public string Cpf { get => cpf; set => cpf = value; }
+        public string Senha { get => senha; set => senha = value; }
 
-        public string CadastroCliente()
+        public string Entra(string cpf, string senha)
         {
-           Usuario user = new Usuario();
+            
+            MySqlConnection con = new MySqlConnection(conexao);
+            MySqlCommand qry = new MySqlCommand("SELECT FROM Usuario(CPF) WHERE = '@cpf'", con);
+          
+
+            MySqlDataReader leitor = qry.ExecuteReader();
+
+            try{
+                
+
+            }
+            catch (Exception e)
+            {
+                return 
+            }
+            
 
 
-        }
+            try
+            {
+                //abre a conexão
+                con.Open();
 
-        public string EntraCliente()
-        {
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
 
-        }
 
-        public string EntraAdm()
-        { 
-        
         }
     }
 }
