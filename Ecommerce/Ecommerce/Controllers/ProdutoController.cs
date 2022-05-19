391d44ab930a109d32289410454c5a1390a23dbb
aluno <aluno@ESN509D1204970>
2022-05-19 13:45:17 -0300

    19/05

--- Ecommerce/Ecommerce/Controllers/ProdutoController.cs
+++ Ecommerce/Ecommerce/Controllers/ProdutoController.cs
@@ -20,34 +20,33 @@ namespace Ecommerce.Controllers
         [HttpPost]
         public IActionResult CadastroProduto(string nome, double preco, string descricao, int cod, int qtd, string img)
         {
-            IFormFile arquivo = Request.Form.Files[0];
-            string tipoArquivo = arquivo.ContentType;
-            if (tipoArquivo.Contains("png") ||
-                    tipoArquivo.Contains("jpeg"))
+            foreach (IFormFile arquivo in Request.Form.Files)
             {
-                MemoryStream s = new MemoryStream();
-                arquivo.CopyToAsync(s);
-                byte[] bytesArquivo = s.ToArray();
-                Produto p = new Produto(nome, preco, descricao, cod, qtd, bytesArquivo);
-                p.Cadastro(nome, preco, descricao, cod, qtd, bytesArquivo);
+                string tipoArquivo = arquivo.ContentType;
+                if (tipoArquivo.Contains("png") ||
+                        tipoArquivo.Contains("jpeg"))
+                {
+                    MemoryStream s = new MemoryStream();
+                    arquivo.CopyToAsync(s);
+                    byte[] bytesArquivo = s.ToArray();
+                    Produto p = new Produto(nome, preco, descricao, cod, qtd, bytesArquivo);
+                    p.Cadastro(nome, preco, descricao, cod, qtd, bytesArquivo);
 
+                }
             }
-
-
-
+           
+            
                 //retorna a view principal
                 return RedirectToAction("CadastroProduto");
         }
 
 
-        /* public IActionResult Lista()
+         public IActionResult Lista()
         {
             return View("Produtos");
         }
 
-        */
-
-        
+      
 
         // [HttpPost]
         /* public IActionResult Lista()
